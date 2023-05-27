b3c05bc4df17e9e1b735df08d958a50c7eed8786
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-05-27 23:09:54 +0530

    create consult page

--- cyster/src/App.js
+++ cyster/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
 import Login from './pages/Login/Login';
+import Appointment from './pages/Appointment/Appointment';
+import Consult from './pages/Consult/Consult';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
@@ -13,6 +15,12 @@ function App() {
         </Routes>
         <Routes>
           <Route exact path='/login' element={<Login/>} />
+        </Routes>
+        <Routes>
+          <Route exact path='/appointment' element={<Appointment/>} />
+        </Routes>
+        <Routes>
+          <Route exact path='/consult' element={<Consult/>} />
         </Routes>
       </Router>
     </div>
---
+++ cyster/src/pages/Consult/Consult.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import { Link } from "react-router-dom";
+
+const Consult = () => {
+    const [doctors, setDoctors] = useState([]);
+  
+    useEffect(() => {
+      // Fetch doctors from MongoDB
+      const fetchDoctors = async () => {
+        const response = await fetch('http://localhost:5005/doctor/');
+        const doctors = await response.json();
+        setDoctors(doctors);
+      };
+  
+      fetchDoctors();
+    }, []);
+  
+    return (
+      <div className='consult'>
+        <Navbar/>
+        <div className='consult_list'>
+          <h1 className='consult_main_head'>CONSULT OUR DOCTORS</h1>
+  
+          {doctors.map((doctor) => (
+            <div className='consult_container' key={doctor.id}>
+              <h3>{doctor.name}</h3>
+              <span>{doctor.speciality}</span>
+              <br />
+              <button className='consult_button'><Link to="/appointment">Book an Appointment</Link></button>
+            </div>
+          ))}
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
+  
+  export default Consult
