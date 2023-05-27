42c384a9f04bc9277353d7347144321970f5131f
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-05-27 23:46:30 +0530

    create appointment data

--- cyster/src/pages/Appointment/Appointment.js
+++ cyster/src/pages/Appointment/Appointment.js
@@ -1,6 +1,4 @@
 import React,{ useState, useEffect } from 'react';
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
 
 const Appointment = () => {
   const [date, setDate] = useState('');
@@ -10,7 +8,7 @@ const Appointment = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-      const response = await fetch('http://localhost:5005/appointment/', {
+      const response = await fetch('http://localhost:5005/appointment', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -27,13 +25,119 @@ const Appointment = () => {
         console.log(responseData.error)
         throw new Error(responseData.error);
       }else{
-        console.log("login successful");
+        console.log("appointment successful");
       }
       
   };
 
+  const handleTime = (e) => {
+    setTime(e.target.value);
+  };
+
   return (
-    <div>Appointment</div>
+    <div className='main_appointment'>
+      <div className="appointment_form">
+      <input
+            type='text'
+            placeholder='Doctor'
+            value={doctor}
+            onChange={(e) => setDoctor(e.target.value)}
+          />
+        <input
+            type='text'
+            placeholder='Date'
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <input
+            type='text'
+            placeholder='Status'
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          />
+
+<div className='mor_slots'>
+          <h1>Morning 09:00 AM to 12:00 PM</h1>
+
+          <input
+          type='radio'
+          name='time'
+          value='09:00'
+          checked={time === '09:00'}
+          onChange={handleTime}
+        />
+          <label>09:00</label>
+
+          <input
+          type='radio'
+          name='time'
+          value='10:00'
+          checked={time === '10:00'}
+          onChange={handleTime}
+        />
+          <label>10:00</label>
+
+          <input
+          type='radio'
+          name='time'
+          value='11:00'
+          checked={time === '11:00'}
+          onChange={handleTime}
+        />
+          <label>11:00</label>
+
+          <input
+          type='radio'
+          name='time'
+          value='12:00'
+          checked={time === '12:00'}
+          onChange={handleTime}
+        />
+          <label>12:00</label>
+          </div>
+
+          <div className='eve_slots'>
+          <h1>Evening 05:00 PM to 08:00 PM</h1>
+
+          <input
+          type='radio'
+          name='time'
+          value='05:00'
+          checked={time === '05:00'}
+          onChange={handleTime}
+        />
+          <label>05:00</label>
+
+          <input
+          type='radio'
+          name='time'
+          value='06:00'
+          checked={time === '06:00'}
+          onChange={handleTime}
+        />
+          <label>06:00</label>
+
+          <input
+          type='radio'
+          name='time'
+          value='07:00'
+          checked={time === '07:00'}
+          onChange={handleTime}
+        />
+          <label>07:00</label>
+
+          <input
+          type='radio'
+          name='time'
+          value='08:00'
+          checked={time === '08:00'}
+          onChange={handleTime}
+        />
+          <label>08:00</label>
+          </div>
+          <button type='submit' onClick={handleSubmit}>Submit</button>
+      </div>
+    </div>
   )
 }
 
