e269dfbfaa17e47489b3ffa1c4300e96f73e80f8
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-05-27 13:03:17 +0530

    create navbar list

---
+++ cyster/src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className='main_nav'>
+            <div className='element_container'>
+                <ul>
+                    <li>Guide</li>
+                    <li>Consult</li>
+                    <li>Dashboard</li>
+                    <li>SignUp/In</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
